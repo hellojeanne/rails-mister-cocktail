b826426ff6f38707648a13df44e445704a98d37e
Jeanne Bouaziz <jeannebouaziz@hotmail.fr>
2019-02-14 16:32:11 +0100

    Mister cokctails done (all routes, controllers, views) without style

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'cocktails/index'
-  get 'cocktails/show'
-  get 'cocktails/new'
-  get 'cocktails/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'cocktails#index'
-  resources :cocktails
+  resources :cocktails do
+    resources :doses, only: [:new, :create]
+  end
+
+  resources :doses, only: [:destroy]
 end
